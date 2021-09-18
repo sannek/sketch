96b16b4d6bac948df93b0ffc36ce04ef5cc0c547
Kate Beard <kate@codesandbox.io>
2021-09-18 14:22:16 +0100

    Add :wx to extra applications and convert int to float for line

--- lib/sketch/primitives/line.ex
+++ lib/sketch/primitives/line.ex
@@ -1,5 +1,5 @@
 defmodule Sketch.Primitives.Line do
-  defstruct [:id, start: {0.0, 0.0}, finish: {10.0, 10.0}]
+  defstruct [:id, start: {0, 0}, finish: {10, 10}]
 
   def new(%{start: start, finish: finish}) do
     %__MODULE__{start: start, finish: finish, id: "line-#{:rand.uniform(100)}"}
@@ -8,6 +8,10 @@ end
 
 defimpl Sketch.Render, for: Sketch.Primitives.Line do
   def render_wx(line, wx_context) do
-    :wxGraphicsContext.drawLines(wx_context, [line.start, line.finish])
+    # Line accepts floats so we need to convert
+    %{start: {startX, startY}, finish: {finishX, finishY}} = line
+    start = {startX / 1, startY / 1}
+    finish = {finishX / 1, finishY / 1}
+    :wxGraphicsContext.drawLines(wx_context, [start, finish])
   end
 end

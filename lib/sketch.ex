76b9ec1466baaada8b0a31e8663eefc48168ee92
Arjan Scherpenisse <arjan@scherpenisse.net>
2021-11-25 10:38:26 +0100

    Move timestamp option inside filename function

--- lib/sketch.ex
+++ lib/sketch.ex
@@ -73,22 +73,24 @@ defmodule Sketch do
 
   """
   def save(%Sketch{} = sketch, opts \\ []) do
-    filename = filename(sketch, "png", Keyword.get(opts, :timestamp, true))
+    filename = filename(sketch, "png", opts)
     Sketch.Render.Png.render(sketch, filename, opts)
   end
 
   def save_svg(%Sketch{} = sketch, opts \\ []) do
-    filename = filename(sketch, "svg", Keyword.get(opts, :timestamp, true))
+    filename = filename(sketch, "svg", opts)
     Sketch.Render.Svg.render(sketch, filename, opts)
   end
 
-  defp filename(sketch, ext, true) do
-    timestamp = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
-    "#{sketch.title}-#{timestamp}.#{ext}"
-  end
+  defp filename(sketch, ext, opts) do
+    case Keyword.get(opts, :timestamp, true) do
+      false ->
+        "#{sketch.title}.#{ext}"
 
-  defp filename(sketch, ext, false) do
-    "#{sketch.title}.#{ext}"
+      _ ->
+        timestamp = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
+        "#{sketch.title}-#{timestamp}.#{ext}"
+    end
   end
 
   def example do
